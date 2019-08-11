a559a4924e03a8ad2dc99f37ad9c537300175d35
Kevin <kevingilbert01@hotmail.com>
2019-08-11 19:39:08 -0400

    first commit

---
+++ Form1/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent();
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e) {
+            MessageBox.Show("My First Message", "Message", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+        }
+    }
+}
